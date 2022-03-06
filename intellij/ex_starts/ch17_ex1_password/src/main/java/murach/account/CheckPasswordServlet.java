0dec791ba97875b6e42e4b067e137f66179a9ae7
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2022-03-05 23:07:25 -0500

    ch17

--- intellij/ex_starts/ch17_ex1_password/src/main/java/murach/account/CheckPasswordServlet.java
+++ intellij/ex_starts/ch17_ex1_password/src/main/java/murach/account/CheckPasswordServlet.java
@@ -1,19 +1,19 @@
 package murach.account;
 
-import java.io.*;
-import java.security.NoSuchAlgorithmException;
-import javax.servlet.*;
-import javax.servlet.http.*;
-
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import murach.util.PasswordUtil;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 public class CheckPasswordServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response)
-            throws ServletException, IOException {
-        
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         // get parameters from the request
         String username = request.getParameter("username");
         String password = request.getParameter("password");
@@ -26,8 +26,8 @@ public class CheckPasswordServlet extends HttpServlet {
         } catch (Exception e) {
             message = e.getMessage();
         }
-        request.setAttribute("message", message);        
-        
+        request.setAttribute("message", message);
+
         // hash and salt password
         String hashedPassword;
         String salt = "";
@@ -35,8 +35,8 @@ public class CheckPasswordServlet extends HttpServlet {
         try {
             hashedPassword = PasswordUtil.hashPassword(password);
             salt = PasswordUtil.getSalt();
-            saltedAndHashedPassword = PasswordUtil.hashAndSaltPassword(password);                    
-            
+            saltedAndHashedPassword = PasswordUtil.hashAndSaltPassword(password);
+
         } catch (NoSuchAlgorithmException ex) {
             hashedPassword = ex.getMessage();
             saltedAndHashedPassword = ex.getMessage();
@@ -44,10 +44,10 @@ public class CheckPasswordServlet extends HttpServlet {
         request.setAttribute("hashedPassword", hashedPassword);
         request.setAttribute("salt", salt);
         request.setAttribute("saltedAndHashedPassword", saltedAndHashedPassword);
-        
+
         String url = "/index.jsp";
         getServletContext()
                 .getRequestDispatcher(url)
                 .forward(request, response);
-    }    
+    }
 }
